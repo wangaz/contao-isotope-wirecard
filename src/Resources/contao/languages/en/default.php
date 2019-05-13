de8939a04433fd2cf963935df84b3dffd1670efd
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-05-13 15:25:20 +0200

    rename to "Wirecard Payment Page"

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -11,4 +11,4 @@ declare(strict_types=1);
  * @license CC-BY-SA-4.0
  */
 
-$GLOBALS['TL_LANG']['MODEL']['tl_iso_payment']['wirecard'] = ['Wirecard'];
+$GLOBALS['TL_LANG']['MODEL']['tl_iso_payment']['wirecard'] = ['Wirecard Payment Page'];
